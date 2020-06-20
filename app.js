4a88126ef5aff03fe9ee37c178b28fb30e1d563e
aknts <25027425+aknts@users.noreply.github.com>
2020-06-20 19:50:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ console.log('Test app for realying mqtt messages between topics.');
 
 // [Settings]
 // Configuration settings
-console.log(process.env.appconfig);
+//var getconfig = process.argv[2];
+//var argconfig = JSON.parse(getconfig);
+//console.log(argconfig);
 //l.log('INFO',process.env.appconfig);
 var config = require('./config.js');
-
+console.log(config);
 // [Libraries]
-const l = require('./logger.js');
-var mqttmod = require('./mqttmod.js');
+const l = require('mqttlogger');
+var mqttmod = require('mqttmod');
 
 // [Functions]
 function sendToNext(msg){
-	console.log(msg);
-	mqttmod.send(config.mqttBroker,config.TxTopic,msg,finalizeSending);
+        console.log(msg);
+        mqttmod.send(config.broker,config.txtopic,msg,finalizeSending);
 }
 
 function finalizeSending() {
-	l.log('INFO','Data sent to heatmap');
-}	
+        l.log('INFO','Data sent to heatmap');
+}
 
 // [Execution]
-mqttmod.receive(config.mqttBroker,config.RxTopic,sendToNext);
+mqttmod.receive(config.broker,config.rxtopic,sendToNext);
 
 // [Ending banner]
 console.log('Code end');
+~
